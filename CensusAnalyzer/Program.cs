fdfcfaa9b22e5a8c2d692b2618c0f6b15aa05d13
“Pratik” <pratikbhandurge2222@gmail.com>
2022-09-16 20:43:31 +0530

    initialcommit

---
+++ CensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
